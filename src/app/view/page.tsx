09aeaa652e7fde2b86d8500bd72b54cded987bbb
Leitnom <juan123m61@gmail.com>
2025-06-04 18:12:19 -0500

    Refactors the application layout and adds new pages. - The use of Geist fonts is eliminated and ‘SF Pro Text’ is implemented. - Sidebar component is integrated and a new layout is established with Ant Design. - New pages are created: Activity, Settings and View, each showing a centered empty component.

---
+++ src/app/view/page.tsx
@@ -0,0 +1,16 @@
+import { Empty } from 'antd';
+
+export default function View() {
+  return (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#F5F5F5',
+    }}>
+        <Empty />
+    </div>
+  );
+}
